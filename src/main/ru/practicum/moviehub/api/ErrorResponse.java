1c91798fd4f927ccde97132b6639d8aa12780460
Barkov <venediktkaverin@gmail.com>
2025-12-23 01:15:49 +0300

    инициализировать details по умолчанию сделать один универсальный метод toJson исправить GSON на компактный

--- src/main/ru/practicum/moviehub/api/ErrorResponse.java
+++ src/main/ru/practicum/moviehub/api/ErrorResponse.java
@@ -1,11 +1,12 @@
 package ru.practicum.moviehub.api;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ErrorResponse {
 
     private String error;
-    private List<String> details;
+    private List<String> details = new ArrayList<>();
 
     public ErrorResponse() {
     }
